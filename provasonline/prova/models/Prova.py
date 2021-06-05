9ae522b1c0e88981aa505b7364c20f8b5f6301a9
Geovanna Menegasse <48860848+geovannamenegasse@users.noreply.github.com>
2021-06-05 15:24:38 -0300

    criação das tabelas de prova

--- provasonline/prova/models/Prova.py
+++ provasonline/prova/models/Prova.py
@@ -1,9 +1,50 @@
 from provasonline import db
 
-class Prova(): # (db.Model):
+class Prova(db.Model):
 
-    # __tablename__ = 'prova'
-    # id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'prova'
+    id          = db.Column(db.Integer, primary_key = True)
+    descricao   = db.Column(db.Text, nullable = True)
+    data        = db.Column(db.Date, nullable = False)
+    
+    def __init__(self, data, descricao):
+        self.data      = data
+        self.descricao = descricao
 
-    pass
 
+class Pergunta(db.Model):
+
+    __tablename__ = 'pergunta'
+    id          = db.Column(db.Integer, primary_key = True)
+    descricao   = db.Column(db.Text, nullable = False)
+    prova       = db.Column(db.Integer, db.ForeignKey('prova.id', ondelete = 'CASCADE'), nullable = False)
+    
+    def __init__(self, descricao, prova):
+        self.descricao = descricao
+        self.prova     = prova
+
+class Opcao(db.Model):
+
+    __tablename__ = 'opcao'
+    id          = db.Column(db.Integer, primary_key = True)
+    descricao   = db.Column(db.Text, nullable = False)
+    correta     = db.Column(db.Boolean, nullable = False)
+    pergunta    = db.Column(db.Integer, db.ForeignKey('pergunta.id', ondelete = 'CASCADE'), nullable = False)
+    
+    def __init__(self, descricao, correta, pergunta):
+        self.descricao = descricao
+        self.correta   = correta
+        self.pergunta  = pergunta
+
+class Resposta(db.Model):
+
+    __tablename__ = 'resposta'
+    id         = db.Column(db.Integer, primary_key = True)
+    pergunta   = db.Column(db.Integer, db.ForeignKey('pergunta.id', ondelete = 'CASCADE'), nullable = False)
+    opcao      = db.Column(db.Integer, db.ForeignKey('opcao.id', ondelete = 'CASCADE'), nullable = False)
+    # aluno      = db.Column(db.Integer, db.ForeignKey('aluno.id', ondelete = 'CASCADE'), nullable = False)
+    
+    def __init__(self, pergunta, opcao): # , aluno
+        self.pergunta = pergunta
+        self.opcao    = opcao
+        # self.aluno    = aluno
